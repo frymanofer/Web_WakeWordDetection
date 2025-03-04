fd1076bff1aee80ef3711387a314a80a9427de75
Ofer Fryman <ofer@getecho.io>
2025-03-04 11:52:58 +0200

    Fix wrong model loaded

--- example/example.js
+++ example/example.js
@@ -16,7 +16,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   const onKeywordDetected = (detected) => {
       if (detected) {
-
         keywordDetector.stopListening();
 
         console.log('Keyword detected \nprediction: ' + detected.prediction);
@@ -27,22 +26,9 @@ document.addEventListener('DOMContentLoaded', async () => {
       }
     };
     const modelsFolderPath = "./models"
+    /* Alternative for a single model 
     const modelToUse = "need_help_now.onnx";
-    statusElement.textContent = 'Loading models...' + modelToUse.replace(/\.onnx$/, '').replace(/_/g, ' ');
-// DO PATH JOIN INSTEAD OF STRING
 
-// const keywordDetector = new KeywordDetector("path/to/models", "model1.onnx", 0.5, 10, (keyword) => console.log("Detected:", keyword));
-/* new API: 
-const modelParamsArr = [
-  { modelToUse: "model1.onnx", threshold: 0.5, bufferCount: 10, onKeywordDetected: (keyword) => console.log("Detected in Model 1:", keyword) },
-  { modelToUse: "model2.onnx", threshold: 0.7, bufferCount: 20, onKeywordDetected: (keyword) => console.log("Detected in Model 2:", keyword) },
-];
-
-const keywordDetector = new KeywordDetector("path/to/models", modelParamsArr, "./dist/", "./dist/");
-
-*/
-
-/* Works Single : 
     const keywordDetector = new KeywordDetector(
       modelsFolderPath,
       modelToUse,
@@ -53,12 +39,15 @@ const keywordDetector = new KeywordDetector("path/to/models", modelParamsArr, ".
     );
 
     */
-     /* New */
+
+     /* For multi model / and single model */
      
     const modelParamsArr = [
       { modelToUse: "need_help_now.onnx", threshold: threshold, bufferCount: bufferCount, onKeywordDetected: onKeywordDetected },
-      { modelToUse: "salut_mia_model_28_20012025.onnx", threshold: threshold, bufferCount: bufferCount, onKeywordDetected: onKeywordDetected },
+// Add more models      { modelToUse: "salut_mia_model_28_20012025.onnx", threshold: threshold, bufferCount: bufferCount, onKeywordDetected: onKeywordDetected },
     ];
+    statusElement.textContent = 'Loading models: ' + 
+    modelParamsArr.map(m => m.modelToUse.replace(/\.onnx$/, '').replace(/_/g, ' ')).join(', ');
     
     const keywordDetector = new KeywordDetector(modelsFolderPath, modelParamsArr, "./dist/", "./dist/");
     
@@ -70,8 +59,9 @@ const keywordDetector = new KeywordDetector("path/to/models", modelParamsArr, ".
    
   try {
     await keywordDetector.init();
-    statusElement.textContent = 'Models loaded. Listening for keywords...' + modelToUse.replace(/\.onnx$/, '').replace(/_/g, ' ');
-
+    statusElement.textContent = 'Models loaded. Listening for keywords...' + 
+    modelParamsArr.map(m => m.modelToUse.replace(/\.onnx$/, '').replace(/_/g, ' ')).join(', ');
+    
     // Start listening for keywords
     keywordDetector.startListening();
   } catch (error) {
